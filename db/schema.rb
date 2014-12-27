99e5a5217f3760ac2ed97eb92b45fe3ef9c43da5
Sean S Jeong <sj@fluxcut.com>
2014-12-27 18:51:25 -0500

    Hm, better to update this now than later. Pray that we don't need to anymore for the time being.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141227033736) do
+ActiveRecord::Schema.define(version: 20141227155621) do
 
   create_table "feedbanks", force: :cascade do |t|
     t.string   "item_id"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20141227033736) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "email"
+    t.string   "email",                    default: "",    null: false
     t.string   "password_digest"
     t.string   "password_confirmation"
     t.string   "remember_token"
@@ -52,6 +52,20 @@ ActiveRecord::Schema.define(version: 20141227033736) do
     t.boolean  "sent_approval",            default: false
     t.datetime "created_at",                               null: false
     t.datetime "updated_at",                               null: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "encrypted_password",       default: "",    null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",            default: 0,     null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
