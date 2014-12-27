97b5dfa11dc6ba2dd068b9dea9c70cbb056ed751
Alan Yuan <alanyuan_ca@yahoo.ca>
2014-12-27 15:42:34 -0500

    copied controller code

--- app/controllers/feedbanks_controller.rb
+++ app/controllers/feedbanks_controller.rb
@@ -1,2 +1,41 @@
 class FeedbanksController < ApplicationController
+
+	def index
+		@currentPage = {:useraccount => "active"};
+		@user_name = "Account Login"
+		@feedbanks = Feedbank.all
+	end
+
+	 def create
+  		@Feedbank = Feedbank.new(params[:feedbank])
+  		@user = User.find_by_id(session[:remember_token])
+
+  		@Feedbank.save
+  		redirect_to @user
+  end
+
+  def show
+    @currentPage = {:useraccount => "active"};
+    @user_name = "Account Login"
+    @feedbanks = Feedbank.all
+  end 
+
+  def approve_content
+    @feedbank  = Feedbank.find(params[:id])
+    @feedbank.update_attribute(:approval_status, "true");
+
+
+    @admin_user = User.find_by_name("Admin");
+    redirect_to @admin_user 
+  end 
+
+  def disapprove_content
+    @feedbank  = Feedbank.find(params[:id])
+    @feedbank.destroy
+
+
+    @admin_user = User.find_by_name("Admin");
+    redirect_to @admin_user 
+  end 
+
 end
--- app/controllers/staticpage_controller.rb
+++ app/controllers/staticpage_controller.rb
@@ -1,15 +1,92 @@
-class StaticpageController < ApplicationController
+class StaticPagesController < ApplicationController
+ 
+  helper_method :login_method
 
-	def home
+  def login_method
+    #helper method to find the cookie login or the user + set the name on the template
 
-	end
+    current_user
 
-	def event
+    if signed_in?
+      sign_in(@current_user)
+    end
 
-	end
+    if session[:remember_token] && User.exists?(:id => session[:remember_token])
+      @user = User.find(session[:remember_token])
+      @user_name = @user.name
+    else
+      @user_name = "Account Login"
+    end
+  end
 
-	def jobs
+  def home
+    login_method
 
-	end
+    @currentPage = {:home => "active"};
+    @number_of_users = User.all.length;
+  end
 
+  def developer
+    login_method
+
+    @currentPage = {:developer => "active"};
+  end
+
+  def entrepreneur
+    login_method
+
+    @currentPage = {:entrepreneur => "active"};
+    @number_of_participants = User.where(:expo_ticket => true).length;
+  end
+
+  def news
+    login_method
+
+    @currentPage = {:news => "active"};
+    @jobPosts = Feedbank.where(:column_type => 3).order("item_date desc")
+    #1 is Jobs, #2 is Events, #3 is News, #4 is Research
+
+  end
+
+  def events
+    login_method
+
+    @currentPage = {:events => "active"};
+    @jobPosts = Feedbank.where(:column_type => 2).order("item_date desc")
+    #1 is Jobs, #2 is Events, #3 is News, #4 is Research
+
+  end
+
+  def jobs
+    login_method
+
+    @currentPage = {:jobs => "active"};
+    @jobPosts = Feedbank.where(:column_type => [1,4]).order("item_date desc")
+    #1 is Jobs, #2 is Events, #3 is News, #4 is Research
+  end
+
+  def useraccount
+    current_user
+
+    if signed_in?
+      sign_in(@current_user)
+    end
+
+    @currentPage = {:useraccount => "active"};
+    if session[:remember_token] && User.exists?(:id => session[:remember_token])
+      @user = User.find(session[:remember_token])
+      @user_name = @user.name
+      redirect_to @user
+    else
+      @user_name = "Account Login"
+      @user = User.new(params[:user])
+      if @user.save
+        #Handle a successful save.
+        redirect_to @user
+      else 
+        render 'users/new'
+      end
+    end
+  end
+  
 end
