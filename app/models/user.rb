727387a4e6aa91e53d0e1bc43d8a88159bb8c66f
Sean S Jeong <sj@fluxcut.com>
2015-01-01 23:52:57 -0500

    makes migration!

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,16 @@ class User < ActiveRecord::Base
   has_one :account_setting
 
   validates :name,  presence: true
+  alias :devise_valid_password? :valid_password?
 
+  def valid_password?(password)
+    begin
+      super(password)
+    rescue BCrypt::Errors::InvalidHash
+      return false unless BCrypt::Password.new(password) == encrypted_password
+      logger.info "User #{email} is using the old password hashing method, updating attribute."
+      self.password = password
+      true
+    end
+  end
 end
