7ed4b2a871e19d10ad369158510966432d79278b
Alan Yuan <alanyuan_ca@yahoo.ca>
2015-01-01 21:19:35 -0500

    models added

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,5 @@ class User < ActiveRecord::Base
   has_one :account_setting
 
   validates :name,  presence: true
-  validates :email_frequency, presence: true, :numericality => {:only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 7 }
-
-  private 
 
 end
