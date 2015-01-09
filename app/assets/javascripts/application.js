2ed1bbee8359eed22b811008e2d664c5efe8c04a
Sean S Jeong <sj@fluxcut.com>
2015-01-08 23:14:58 -0500

    refresh bug fixed, I think!

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
+//= require jquery_ujs
 //= require turbolinks
 //= require ckeditor/init
-//= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
