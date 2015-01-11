3bd0a698a46177d1b47c1152bd845d4545161d8a
Sean S Jeong <sj@fluxcut.com>
2015-01-11 17:14:11 -0500

    csrf wrong place? I think so

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -25,19 +25,6 @@ CKEDITOR.editorConfig = function( config ) {
     // The location of a script that handles file uploads.
     config.filebrowserUploadUrl = "/ckeditor/attachment_files";
 
-    // Rails CSRF token
-    config.filebrowserParams = function(){
-        var csrf_token = $('meta[name=csrf-token]').attr('content'),
-                csrf_param = $('meta[name=csrf-param]').attr('content'),
-                params = new Object();
-
-        if (csrf_param !== undefined && csrf_token !== undefined) {
-            params[csrf_param] = csrf_token;
-        }
-
-        return params;
-    };
-
 
     config.toolbar_toolbarLight =
     [
