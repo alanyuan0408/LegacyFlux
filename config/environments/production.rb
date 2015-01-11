6183b6fc7b14a04d3f3766f17e9218f9f3e328b7
Sean S Jeong <sj@fluxcut.com>
2015-01-11 16:53:48 -0500

    ckeditor hack

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,9 +40,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   # CKedit dirty hack
-  config.assets.compile = false
-  config.assets.precompile += Ckeditor.assets
-
+  config.assets.compile = true
+  config.serve_static_assets = true
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
