3623107735baddc3809895024f37ab0ab022777c
Sean S Jeong <sj@fluxcut.com>
2015-01-11 16:27:15 -0500

    cancan integration -- half way done // must be tested on heroku

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -3,6 +3,40 @@ CKEDITOR.editorConfig = function( config ) {
     config.uiColor = '#ffffff';
 	config.skin = 'office2013,./office2013/';
     config.toolbar = 'toolbarLight';
+    /* Filebrowser routes */
+    // The location of an external file browser, that should be launched when "Browse Server" button is pressed.
+    config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
+
+    // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Flash dialog.
+    config.filebrowserFlashBrowseUrl = "/ckeditor/attachment_files";
+
+    // The location of a script that handles file uploads in the Flash dialog.
+    config.filebrowserFlashUploadUrl = "/ckeditor/attachment_files";
+
+    // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Link tab of Image dialog.
+    config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
+
+    // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Image dialog.
+    config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
+
+    // The location of a script that handles file uploads in the Image dialog.
+    config.filebrowserImageUploadUrl = "/ckeditor/pictures";
+
+    // The location of a script that handles file uploads.
+    config.filebrowserUploadUrl = "/ckeditor/attachment_files";
+
+    // Rails CSRF token
+    config.filebrowserParams = function(){
+        var csrf_token = $('meta[name=csrf-token]').attr('content'),
+                csrf_param = $('meta[name=csrf-param]').attr('content'),
+                params = new Object();
+
+        if (csrf_param !== undefined && csrf_token !== undefined) {
+            params[csrf_param] = csrf_token;
+        }
+
+        return params;
+    };
 
 
     config.toolbar_toolbarLight =
