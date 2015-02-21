94d4583c508af7538d19fa86c6435602aa789113
Alan Yuan <alanyuan_ca@yahoo.ca>
2015-02-21 00:53:17 -0500

    remvoed unneccesary file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery-ui/slider
+//= require jquery-ui/draggable
+//= require jquery-ui/droppable
 //= require jquery_ujs
 //= require ckeditor/init
 //= require turbolinks
