8c501d3e3f6189035b1c86653baf9ba4d4f2e147
Sean S Jeong <sj@fluxcut.com>
2015-04-15 17:22:35 -0400

    dropping ckeditor

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,17 +2,6 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-  can :access, :ckeditor   # needed to access Ckeditor filebrowser
-
-	# Performed checks for actions:
-	user ||= User.new 
-	if user.account_setting.news_admin
-		can [:read, :create, :destroy], Ckeditor::Picture
-		can [:read, :create, :destroy], Ckeditor::AttachmentFile
-	else
-		can [:read, :create], Ckeditor::Picture
-		can [:read, :create], Ckeditor::AttachmentFile
-	end
 	# Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
