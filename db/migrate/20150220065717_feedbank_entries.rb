08e9750f39b7845294fcdb16e3579dc6026fec05
Alan Yuan <alanyuan_ca@yahoo.ca>
2015-04-26 12:44:19 -0400

    fixed migration

--- db/migrate/20150220065717_feedbank_entries.rb
+++ db/migrate/20150220065717_feedbank_entries.rb
@@ -2,7 +2,7 @@ class FeedbankEntries < ActiveRecord::Migration
   def change
   	add_column :feedbanks, :disapproval_reason, :text
 	#add_column :news_letter_entries, :entry_text_md, :text
-	add_column :news_letter_mails, :intro_message_md, :text
+	#add_column :news_letter_mails, :intro_message_md, :text
 
 	add_column :feedbanks, :important, :boolean, :default => false
 	add_column :feedbanks, :cs_grad, :boolean, :default => false
