6374436104e2c5b37e29ac9346e560d2d33a5391
Alan Yuan <alanyuan_ca@yahoo.ca>
2015-04-26 12:41:55 -0400

    working true

--- db/migrate/20150420220604_add_attributes.rb
+++ db/migrate/20150420220604_add_attributes.rb
@@ -2,10 +2,10 @@ class AddAttributes < ActiveRecord::Migration
   def change
   	add_column :feedbanks, :content_modification, :boolean, :default => false
 
-  	add_column :mail_settings, :full_time_job, :boolean, :default => false
-	add_column :mail_settings, :Internship_job, :boolean, :default => false
-	add_column :mail_settings, :Research_job, :boolean, :default => false
-	add_column :mail_settings, :Part_time_job, :boolean, :default => false 
+  	add_column :mail_settings, :full_time_job, :boolean, :default => true
+	add_column :mail_settings, :Internship_job, :boolean, :default => true
+	add_column :mail_settings, :Research_job, :boolean, :default => true
+	add_column :mail_settings, :Part_time_job, :boolean, :default => true 
 	remove_column :mail_settings, :jobs
 	remove_column :mail_settings, :research
 	remove_column :mail_settings, :expo_ticket
